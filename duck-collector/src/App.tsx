c1e8242f7e71092122812d0714611fabe6b6f728
ShawnSomething <khooweishawn@gmail.com>
2024-10-03 16:22:44 +1000

    1st duckling spawn

--- duck-collector/src/App.tsx
+++ duck-collector/src/App.tsx
@@ -1,20 +1,26 @@
-import React, {useState} from 'react';
-import './App.css';
-import { GameBoard } from './game-board';
-import { GameStart } from './game-start';
-import { MamaDuck } from './mama-duck';
+import React, { useState } from "react";
+import "./App.css";
+import { GameBoard } from "./game-board";
+import { GameStart } from "./game-start";
+import { MamaDuck } from "./mama-duck";
+import { Ducklings } from "./ducklings";
 
 function App() {
-  const [gameStarted, setGameStarted] = useState(false)
+  const [gameStarted, setGameStarted] = useState(false);
 
   return (
     <>
       <div>
         <GameBoard />
-        
+
         {!gameStarted && <GameStart onStart={() => setGameStarted(true)} />}
-        
-        {gameStarted && <MamaDuck />}
+
+        {gameStarted && (
+          <>
+            <MamaDuck />
+            <Ducklings gameStarted={gameStarted} />
+          </>
+        )}
       </div>
     </>
   );
--- duck-collector/src/ducklings.tsx
+++ duck-collector/src/ducklings.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export const Ducklings = () => {
-    return (
-        <header>ducklings</header>
-    )
-}
+export const Ducklings: React.FC<{ gameStarted: boolean }> = ({
+  gameStarted,
+}) => {
+  const [ducklingPosition, setDucklingPosition] = useState<{
+    left: Number;
+    top: number;
+  } | null>(null);
+
+  const spawnDuckling = () => {
+    const ducklingWidth = 20;
+    const ducklingHeight = 20;
+
+    const boardWidth = window.innerWidth;
+    const boardHeight = window.innerHeight;
+
+    const randomLeft = Math.random() * (boardWidth - ducklingWidth);
+    const randomTop = Math.random() * (boardHeight - ducklingHeight);
+
+    setDucklingPosition({ left: randomLeft, top: randomTop });
+  };
+
+  useEffect(() => {
+    if (gameStarted) {
+      spawnDuckling();
+    }
+  }, [gameStarted]);
+
+  return (
+    <>
+      {ducklingPosition && (
+        <div
+          className="ducklings"
+          style={{
+            position: "absolute",
+            left: `${ducklingPosition.left}px`,
+            top: `${ducklingPosition.top}px`,
+          }}
+        ></div>
+      )}
+    </>
+  );
+};
