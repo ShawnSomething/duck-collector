bb47d7bd2adf114b640d04cd853325b9f25f9523
ShawnSomething <khooweishawn@gmail.com>
2024-10-12 11:51:14 +1100

    get wolf back

--- duck-collector/src/App.tsx
+++ duck-collector/src/App.tsx
@@ -110,7 +110,16 @@ function App() {
               onCollision={handleCollision}
               mamaDuckPosition={mamaDuckPosition}
             />
-
+            {wolves.map((wolfPosition, index) => (
+              <Wolf
+                key={index}
+                gameStarted={gameStarted}
+                collectedDucklings={collectedDucklings}
+                mamaDuckPosition={mamaDuckPosition}
+                onEating={handleEating}
+                wolfIndex={index}
+              />
+            ))}
             <Butcher
               gameStarted={gameStarted}
               collectedDucklings={collectedDucklings}
